74b6a420cb1132f1cfa4cc4455e423365bf6b8e5
Alex Smirnov <alex-wd@yandex.ru>
2022-01-28 15:52:28 +0300

    Initial commit

---
+++ js/common.js
@@ -0,0 +1,49 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const swiper = new Swiper(".swiper", {
+    direction: "horizontal",
+    slidesPerView: 4,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    breakpoints: {
+      320: {
+        slidesPerView: 1,
+      },
+
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 30,
+      },
+
+      1024: {
+        slidesPerView: 3,
+        spaceBetween: 30,
+      },
+
+      1400: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+    },
+
+    navigation: {
+      nextEl: ".swiper-next",
+      prevEl: ".swiper-prev",
+    },
+  });
+
+  const carItemsSlides1 = new Swiper(".car-item__slider", {
+    direction: "horizontal",
+    slidesPerView: 1,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    navigation: {},
+  });
+});
